0b8e662f0f6f9575e4f72d586df0bc1700bbf2fa
ShahbozQurbonov <shahbozq571@gmail.com>
2025-03-18 11:59:28 +0500

    commit izohi 1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,6 +13,10 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
+
+
+
+
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email',
